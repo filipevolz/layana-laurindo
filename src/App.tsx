5fa44019813c3cec0732c5dd23d773112cb63b4a
Filipe Volz <devfilipevolz@gmail.com>
2026-06-18 16:37:49 -0300

    add speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { AboutSection } from '@/components/sections/AboutSection'
 import { ContactSection } from '@/components/sections/ContactSection'
 import { FaqSection } from '@/components/sections/FaqSection'
@@ -26,6 +27,7 @@ function App() {
       <Footer />
       <WhatsAppButton />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
